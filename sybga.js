d107a9ba04c64a53eb7eb9496f06cc624616416c
Alexander Rühl <alexander.ruehl@syrocon.de>
2021-02-01 21:59:38 +0100

    notification callbacks and animation

--- sybga.js
+++ sybga.js
@@ -69,7 +69,7 @@ function (dojo, declare) {
             }
  
             dojo.query( '.square' ).connect( 'onclick', this, 'onPlayDisc' );
-            
+
             // Setup game notifications to handle (see "setupNotifications" method below)
             this.setupNotifications();
 
@@ -301,6 +301,14 @@ function (dojo, declare) {
             // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
             // this.notifqueue.setSynchronous( 'cardPlayed', 3000 );
             // 
+
+            dojo.subscribe( 'playDisc', this, "notif_playDisc" );
+            this.notifqueue.setSynchronous( 'playDisc', 500 );
+            dojo.subscribe( 'turnOverDiscs', this, "notif_turnOverDiscs" );
+            this.notifqueue.setSynchronous( 'turnOverDiscs', 1500 );
+            dojo.subscribe( 'newScores', this, "notif_newScores" );
+            this.notifqueue.setSynchronous( 'newScores', 500 );
+
         },  
         
         // TODO: from this point and below, you can write your game notifications handling methods
@@ -319,5 +327,57 @@ function (dojo, declare) {
         },    
         
         */
+
+       notif_playDisc: function( notif )
+       {
+           // Remove current possible moves (makes the board more clear)
+           dojo.query( '.possibleMove' ).removeClass( 'possibleMove' );        
+       
+           this.addTokenOnBoard( notif.args.x, notif.args.y, notif.args.player_id );
+       },
+
+       notif_turnOverDiscs: function( notif )
+        {
+            // Get the color of the player who is returning the discs
+            var targetColor = this.gamedatas.players[ notif.args.player_id ].color;
+
+            // Make these discs blink and set them to the specified color
+            for( var i in notif.args.turnedOver )
+            {
+                var token = notif.args.turnedOver[ i ];
+                
+                // Make the token blink 2 times
+                var anim = dojo.fx.chain( [
+                    dojo.fadeOut( { node: 'token_'+token.x+'_'+token.y } ),
+                    dojo.fadeIn( { node: 'token_'+token.x+'_'+token.y } ),
+                    dojo.fadeOut( { 
+                                    node: 'token_'+token.x+'_'+token.y,
+                                    onEnd: function( node ) {
+
+                                        // Remove any color class
+                                        dojo.removeClass( node, [ 'tokencolor_000000', 'tokencolor_ffffff' ] );
+                                        // ... and add the good one
+                                        dojo.addClass( node, 'tokencolor_'+targetColor );
+                                                             
+                                    } 
+                                  } ),
+                    dojo.fadeIn( { node: 'token_'+token.x+'_'+token.y  } )
+                                 
+                ] ); // end of dojo.fx.chain
+
+                // ... and launch the animation
+                anim.play();                
+            }
+        },
+
+        notif_newScores: function( notif )
+        {
+            for( var player_id in notif.args.scores )
+            {
+                var newScore = notif.args.scores[ player_id ];
+                this.scoreCtrl[ player_id ].toValue( newScore );
+            }
+        },
+        
    });             
 });
